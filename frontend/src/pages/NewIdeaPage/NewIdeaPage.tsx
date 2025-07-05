c25147eb32ebb19806a6592fa7f86a1af7712e9f
agneja00 <agnieshaa00@gmail.com>
2025-07-05 13:00:00 +0300

    Added formik

--- frontend/src/pages/NewIdeaPage/NewIdeaPage.tsx
+++ frontend/src/pages/NewIdeaPage/NewIdeaPage.tsx
@@ -1,8 +1,35 @@
+import { useFormik } from "formik";
+import { Input } from "../../components/Input/Input";
+import { Segment } from "../../components/Segment/Segment";
+import { Textarea } from "../../components/Textarea/Textarea";
+
 export const NewIdeaPage = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      nick: "",
+      description: "",
+      text: "",
+    },
+    onSubmit: (values) => {
+      console.info("Submitted", values);
+    },
+  });
+
   return (
-    <div>
-      <h1>New Idea</h1>
-      <div>Form will be here...</div>
-    </div>
+    <Segment title="New Idea">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          formik.handleSubmit();
+        }}
+      >
+        <Input name="name" label="Name" formik={formik} />
+        <Input name="nick" label="Nick" formik={formik} />
+        <Input name="description" label="Description" formik={formik} />
+        <Textarea name="text" label="Text" formik={formik} />
+        <button type="submit">Create Idea</button>
+      </form>
+    </Segment>
   );
 };
