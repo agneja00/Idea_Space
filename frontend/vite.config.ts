6b0bd2b743af6216e7317d788c64fdc5aa65dac3
agneja00 <agnieshaa00@gmail.com>
2025-07-10 12:20:59 +0300

    Created environment variables for the frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,33 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @use "include-media" as *;
-          @use "reset-css/sass/reset" as *;
-          @use "@/styles/_vars" as *;
-          @use "@/styles/_mixins" as *;
-        `,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    resolve: {
+      alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "include-media" as *;
+            @use "reset-css/sass/reset" as *;
+            @use "@/styles/_vars" as *;
+            @use "@/styles/_mixins" as *;
+          `,
+        },
       },
     },
-  },
+    server: {
+      port: Number(env.PORT) || 5173,
+    },
+    preview: {
+      port: Number(env.PORT) || 5173,
+    },
+  };
 });
