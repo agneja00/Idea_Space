bcd3a66cbdd636396e304cc435b149668806301d
agneja00 <agnieshaa00@gmail.com>
2025-07-21 19:39:20 +0300

    Added infinite data loading on button press

--- frontend/src/pages/ideas/AllIdeasPage/AllIdeasPage.tsx
+++ frontend/src/pages/ideas/AllIdeasPage/AllIdeasPage.tsx
@@ -1,37 +1,60 @@
 import css from "./AllIdeasPage.module.scss";
 import { Segment } from "../../../components/Segment/Segment";
 import { Link } from "react-router-dom";
+import { Alert } from "../../../components/Alert/Alert";
 import { getViewIdeaRoute } from "../../../lib/routes";
 import { trpc } from "../../../lib/trpc";
 
 export const AllIdeasPage = () => {
-  const { data, error, isLoading, isFetching, isError } = trpc.getIdeas.useQuery();
-
-  if (isLoading || isFetching) {
-    return <p>Loading...</p>;
-  }
-
-  if (isError) {
-    return <p>Error: {error.message}</p>;
-  }
+  const { data, error, isLoading, isError, hasNextPage, fetchNextPage, isFetchingNextPage, isRefetching } =
+    trpc.getIdeas.useInfiniteQuery(
+      {
+        limit: 2,
+      },
+      {
+        getNextPageParam: (lastPage) => {
+          return lastPage.nextCursor;
+        },
+      },
+    );
 
   return (
     <Segment title="All Ideas">
-      <div className={css.ideas}>
-        {data?.ideas.map((idea) => (
-          <div className={css.idea} key={idea.nick}>
-            <Segment
-              size={2}
-              title={
-                <Link className={css.ideaLink} to={getViewIdeaRoute({ ideaNick: idea.nick })}>
-                  {idea.name}
-                </Link>
-              }
-              description={idea.description}
-            />
+      {isLoading || isRefetching ? (
+        <div>Loading...</div>
+      ) : isError ? (
+        <Alert color="red">{error.message}</Alert>
+      ) : (
+        <div className={css.ideas}>
+          {data?.pages
+            .flatMap((page) => page.ideas)
+            .map((idea) => (
+              <div className={css.idea} key={idea.nick}>
+                <Segment
+                  size={2}
+                  title={
+                    <Link className={css.ideaLink} to={getViewIdeaRoute({ ideaNick: idea.nick })}>
+                      {idea.name}
+                    </Link>
+                  }
+                  description={idea.description}
+                />
+              </div>
+            ))}
+          <div className={css.more}>
+            {hasNextPage && !isFetchingNextPage && (
+              <button
+                onClick={() => {
+                  void fetchNextPage();
+                }}
+              >
+                Load more
+              </button>
+            )}
+            {isFetchingNextPage && <span>Loading...</span>}
           </div>
-        ))}
-      </div>
+        </div>
+      )}
     </Segment>
   );
 };
