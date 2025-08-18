10d26f3df99713df29f3b4c485bd143e906a0b5c
agneja00 <agnieshaa00@gmail.com>
2025-08-18 18:10:03 +0300

    Added logger for trpc in backend

--- backend/src/lib/trpc.ts
+++ backend/src/lib/trpc.ts
@@ -6,6 +6,7 @@ import { expressHandler } from "trpc-playground/handlers/express";
 import { type TrpcRouter } from "../router";
 import { type ExpressRequest } from "../utils/types";
 import { type AppContext } from "./ctx";
+import { logger } from "./logger";
 
 const getCreateTrpcContext =
   (appContext: AppContext) =>
@@ -16,9 +17,33 @@ const getCreateTrpcContext =
 
 type TrpcContext = inferAsyncReturnType<ReturnType<typeof getCreateTrpcContext>>;
 
-export const trpc = initTRPC.context<TrpcContext>().create({
-  transformer: superjson,
-});
+const trpc = initTRPC.context<TrpcContext>().create({ transformer: superjson });
+
+export const createTrpcRouter = trpc.router;
+
+export const trpcLoggedProcedure = trpc.procedure.use(
+  trpc.middleware(async ({ path, type, next, ctx, input }) => {
+    const start = Date.now();
+    const result = await next();
+    const durationMs = Date.now() - start;
+
+    const meta = {
+      path,
+      type,
+      userId: ctx.me?.id || null,
+      durationMs,
+      input: input ?? null,
+    };
+
+    if (result.ok) {
+      logger.info(`trpc:${type}:success`, "Successful request", { ...meta, output: result.data });
+    } else {
+      logger.error(`trpc:${type}:error`, result.error, meta);
+    }
+
+    return result;
+  }),
+);
 
 export const applyTrpcToExpressApp = async (expressApp: Express, appContext: AppContext, trpcRouter: TrpcRouter) => {
   expressApp.use(
