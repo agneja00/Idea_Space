280b1261ce12df99512a1d15780970fd95a17550
agneja00 <agnieshaa00@gmail.com>
2025-08-27 12:44:00 +0300

    Added integration tests

--- backend/src/scripts/notifyAboutMostLikedIdeas.ts
+++ backend/src/scripts/notifyAboutMostLikedIdeas.ts
@@ -1,38 +1,43 @@
-import { type Idea } from "@prisma/client";
+import { Prisma, type Idea } from "@prisma/client";
 import { type AppContext } from "../lib/ctx";
 import { sendMostLikedIdeasEmail } from "../lib/emails";
 
-export const notifyAboutMostLikedIdeas = async (ctx: AppContext) => {
-  const mostLikedIdeas = await ctx.prisma.$queryRaw<
-    Array<Pick<Idea, "id" | "nick" | "name"> & { thisMonthLikesCount: number }>
-  >`
+export const getMostLikedIdeas = async (ctx: AppContext, limit = 10, now?: Date) => {
+  const sqlNow = now ? Prisma.sql`${now.toISOString()}::timestamp` : Prisma.sql`now()`;
+
+  return await ctx.prisma.$queryRaw<Array<Pick<Idea, "id" | "nick" | "name"> & { thisMonthLikesCount: number }>>`
     with "topIdeas" as (
-      select id,
-        nick,
-        name,
+      select i.id,
+        i.nick,
+        i.name,
         (
           select count(*)::int
           from "IdeaLike" il
           where il."ideaId" = i.id
-            and il."createdAt" > now() - interval '1 month'
+            and il."createdAt" > ${sqlNow} - interval '1 month'
             and i."blockedAt" is null
         ) as "thisMonthLikesCount"
       from "Idea" i
       order by "thisMonthLikesCount" desc
-      limit 10
+      limit ${limit}
     )
     select *
     from "topIdeas"
     where "thisMonthLikesCount" > 0
   `;
+};
+
+export const notifyAboutMostLikedIdeas = async (ctx: AppContext) => {
+  const mostLikedIdeas = await getMostLikedIdeas(ctx);
+
   if (!mostLikedIdeas.length) {
     return;
   }
+
   const users = await ctx.prisma.user.findMany({
-    select: {
-      email: true,
-    },
+    select: { email: true },
   });
+
   for (const user of users) {
     await sendMostLikedIdeasEmail({ user, ideas: mostLikedIdeas });
   }
