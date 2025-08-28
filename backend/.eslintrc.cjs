27b8d3a14bcd11b7c1a480333e76d4d537bba11d
agneja00 <agnieshaa00@gmail.com>
2025-08-28 16:00:07 +0300

    Added eslint rule for the ban on importing tests into the main code

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -1,17 +1,49 @@
 module.exports = {
   root: true,
   env: {
-    es2023: true,
     node: true,
+    es2023: true,
   },
   parserOptions: {
     ecmaVersion: 2023,
     sourceType: "module",
   },
   extends: ["../.eslintrc.cjs"],
+  plugins: ["import"],
   rules: {
     "no-unused-vars": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/ban-types": "off",
+
+    "import/no-restricted-paths": [
+      "error",
+      {
+        zones: [
+          {
+            target: ["./src/**/!(*.integration.test.ts)"],
+            from: "./src/test/**/*",
+            message: "You cannot import test files into main backend code!",
+          },
+        ],
+      },
+    ],
   },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project: "./tsconfig.json",
+      },
+      node: {
+        extensions: [".ts", ".js"],
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ["src/test/**/*.ts", "src/**/*.integration.test.ts"],
+      rules: {
+        "import/no-restricted-paths": "off",
+      },
+    },
+  ],
 };
