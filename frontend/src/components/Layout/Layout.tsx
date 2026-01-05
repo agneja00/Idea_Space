add7b245648bc2031c1252d6032ea4125a6a989f
agneja00 <agnieshaa00@gmail.com>
2026-01-06 01:50:57 +0200

    Added footer copyright

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import css from "./Layout.module.scss";
 import { ReactComponent as Logo } from "@/assets/images/logo.svg";
 import { createRef } from "react";
 import { Link, Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
 import {
   getAllIdeasRoute,
   getEditProfileRoute,
@@ -18,53 +19,56 @@ export const Layout = () => {
   const me = useMe();
 
   return (
-    <div className={css.layout}>
-      <div className={css.navigation}>
-        <Logo className={css.logo} />
-        <ul className={css.menu}>
-          <li className={css.item}>
-            <Link className={css.link} to={getAllIdeasRoute()}>
-              All Ideas
-            </Link>
-          </li>
+    <>
+      <div className={css.layout}>
+        <div className={css.navigation}>
+          <Logo className={css.logo} />
+          <ul className={css.menu}>
+            <li className={css.item}>
+              <Link className={css.link} to={getAllIdeasRoute()}>
+                All Ideas
+              </Link>
+            </li>
 
-          {me ? (
-            <>
-              <li className={css.item}>
-                <Link className={css.link} to={getNewIdeaRoute()}>
-                  Add Idea
-                </Link>
-              </li>
-              <li className={css.item}>
-                <Link className={css.link} to={getEditProfileRoute()}>
-                  Edit Profile
-                </Link>
-              </li>
-              <li className={css.item}>
-                <Link className={css.link} to={getSignOutRoute()}>
-                  Log Out ({me.nick})
-                </Link>
-              </li>
-            </>
-          ) : (
-            <>
-              <li className={css.item}>
-                <Link className={css.link} to={getSignUpRoute()}>
-                  Sign Up
-                </Link>
-              </li>
-              <li className={css.item}>
-                <Link className={css.link} to={getSignInRoute()}>
-                  Sign In
-                </Link>
-              </li>
-            </>
-          )}
-        </ul>
+            {me ? (
+              <>
+                <li className={css.item}>
+                  <Link className={css.link} to={getNewIdeaRoute()}>
+                    Add Idea
+                  </Link>
+                </li>
+                <li className={css.item}>
+                  <Link className={css.link} to={getEditProfileRoute()}>
+                    Edit Profile
+                  </Link>
+                </li>
+                <li className={css.item}>
+                  <Link className={css.link} to={getSignOutRoute()}>
+                    Log Out ({me.nick})
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className={css.item}>
+                  <Link className={css.link} to={getSignUpRoute()}>
+                    Sign Up
+                  </Link>
+                </li>
+                <li className={css.item}>
+                  <Link className={css.link} to={getSignInRoute()}>
+                    Sign In
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+        <main className={css.content} ref={layoutContentElRef}>
+          <Outlet />
+        </main>
       </div>
-      <main className={css.content} ref={layoutContentElRef}>
-        <Outlet />
-      </main>
-    </div>
+      <Footer />
+    </>
   );
 };
